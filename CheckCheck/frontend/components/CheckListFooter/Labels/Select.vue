14ea5b683604dea716bc81416b95408b36f0fc4c
motey <bleimehl@gmail.com>
2025-07-01 23:20:25 +0200

    label wip

--- CheckCheck/frontend/components/CheckListFooter/Labels/Select.vue
+++ CheckCheck/frontend/components/CheckListFooter/Labels/Select.vue
@@ -1,38 +1,34 @@
-
-
 <template>
-  <USelectMenu
-    @click.stop
-    multiple
-    :items="items"
-    v-model="selected"
-    searchable
-    :ui="{ width: 'w-auto' }"
-    :uiMenu="{ width: 'w-64' }"
-    variant="none"
-  >
-    <!-- Custom Activator: Only Icon Button -->
-    <template #default="{ open, toggle }">
-      <UButton
-        icon="i-lucide-tag"
-        size="sm"
-        color="gray"
-        variant="ghost"
-        @click="toggle"
-        :aria-expanded="open"
-        aria-haspopup="listbox"
-      />
+  <UDropdownMenu searchable :items="items" :content="{ align: 'start' }" :ui="{ content: 'w-48' }">
+    <CheckListColoredButton variant="ghost" icon="i-lucide-tags" :checkListId="checkListId" @click.stop />
+    <template #search-label>
+      <UInput v-model="query" />
     </template>
-  </USelectMenu>
+    <!--<UButton @click.stop color="neutral" variant="outline" icon="i-lucide-menu" />-->
+  </UDropdownMenu>
 </template>
 
+<script setup lang="ts">
+const props = defineProps({
+  checkListId: {
+    type: String,
+    required: true,
+  },
+});
 
-<script setup>
 const items = ref([
+  {
+    icon: "i-lucide-search",
+    slot: "search" as const,
+    onSelect(e: Event) {
+      e.preventDefault();
+    },
+  },
   { label: "Apple 123erd23fr23wefwefwe", value: "apple" },
   { label: "Banana", value: "banana" },
   { label: "Cherry", value: "cherry" },
 ]);
 
 const selected = ref(null);
+const query = ref(null);
 </script>
